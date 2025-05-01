31da7793b59f392136910e01eede7d1ea3e0b736
qhd279 <acccountt.app@gmail.com>
2025-05-01 17:05:30 +0700

    fix responsive UI

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -73,7 +73,13 @@ const ChatContainer = ({
         }
     }, [messages]);
     return (
-        <div className="col-9" style={{ padding: "10px", position: "relative" }}>
+        <div className="col-9" style={{
+            padding: "10px",
+            position: "relative",
+            display: "flex", 
+            flexDirection: "column",
+            height: "100vh", 
+        }}>
             <h3 style={{textAlign:'left'}}>Chat Room: {currentRoom}</h3>
             <button className="btn btn-secondary mb-2" onClick={onGetGroupDetails}>
                 Group Details
