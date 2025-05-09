8ce7cb97e836222209c1704afeb26c9b5757859d
ngophuc29 <ngophuc2911@gmail.com>
2025-05-09 11:59:32 +0700

    fix add gr modal và login lag

--- client/src/context/GlobalProvider.jsx
+++ client/src/context/GlobalProvider.jsx
@@ -5,10 +5,16 @@ const GlobalContext = createContext();
 // eslint-disable-next-line react-refresh/only-export-components
 export const useGlobalContext = () => useContext(GlobalContext);
 
-export default function GlobalProvider({ children }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+export default function GlobalProvider({ children }) {  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {});
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('username');
+    if (token && username) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   return (
     <GlobalContext.Provider
       value={{
